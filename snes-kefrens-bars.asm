9fe8119cbea375429416d95dc6f9b8d1612c37e6
ferris <yupferris@gmail.com>
2017-06-11 21:23:23 +0200

    Almost working

--- snes-kefrens-bars.asm
+++ snes-kefrens-bars.asm
@@ -57,11 +57,13 @@ empty_interrupt_handler:
 
 .ramsection "vars" slot 1
 bar_pos db
+bar_pos_temp db
 .ends
 
-.define ram_chars $0100
-.define scroll_hdma_table $1000
-.define vram_addr_data_hdma_table $1400
+.define scroll_hdma_table $0400
+.define cgram_addr_hdma_table $1000
+.define cgram_data_1_hdma_table $1400
+.define cgram_data_2_hdma_table $1800
 
 .section "entry" semifree
 
@@ -87,6 +89,27 @@ entry:
     ; Clear horizontal scroll reg for bg 0
     stz $210d
     stz $210d
+    ; Clear mode 7 initial settings reg
+    stz $211a
+    ; Clear mode 7 center coord
+    stz $211f
+    stz $211f
+    stz $2120
+    stz $2120
+    ; Set mode 7 identity matrix
+    lda #$01
+    ;  A (1)
+    stz $211b
+    sta $211b
+    ;  B (0)
+    stz $211c
+    stz $211c
+    ;  C (0)
+    stz $211d
+    stz $211d
+    ;  D (1)
+    stz $211e
+    sta $211e
     ; Enable BG0 for main screen
     lda #$01
     sta $212c
@@ -101,40 +124,13 @@ entry:
     ; Clear screen setting reg
     stz $2133
 
-    ; Set graphics mode 0, tile size 8 for all bg's
+    ; Set graphics mode 7
     sep #$20 ; 8-bit a
-    lda #$00
+    lda #$07
     sta $2105
 
-    ; Set background color
-    sep #$30 ; 8-bit a/x/y
-    ; Red BG
-    stz $2121
-    lda #$18
-    sta $2122
-    stz $2122
-    ; Set bar colors
-    lda #$03
-    tax
-palette_loop:
-        stz $2122
-        sta $2122
-        clc
-        adc #$20
-    dex
-    bne palette_loop
-
-    ; Set tile map location to $8000
-    sep #$20 ; 8-bit a
-    lda #$40
-    sta $2107
-
-    ; Set char location to $0000 (for easy updates later)
-    lda #$00
-    sta $210b
-
     ; Clear VRAM
-    sep #$20 ; 8-bit a
+    /*sep #$20 ; 8-bit a
     lda #$80
     sta $2115
     rep #$30 ; 16-bit a/x/y
@@ -144,23 +140,53 @@ clear_vram_loop:
         stz $2118
     inx
     cpx #$8000
-    bne clear_vram_loop
+    bne clear_vram_loop*/
 
-    ; Load tile map
-    ;  We want to display the first row of the first 32 tiles, so we'll just write 0-31 into the first row of the map
+    ; Set up map data
+    ;  We'll do a bunch of 8-bit writes to the low byte of each word in mem in increasing order
+    sep #$30 ; 8-bit a, x, y
+    stz $2115
+    stz $2116
+    stz $2117
+    ldx #$00
+map_data_loop:
+        stx $2118
+    inx
+    cpx #$20
+    bne map_data_loop
+
+    ; Set up tile data
+    ;  For now, just clear tile 0
     sep #$20 ; 8-bit a
     lda #$80
     sta $2115
-    rep #$30 ; 16-bit a/x/y
-    lda #$4000
-    sta $2116
-    lda #$0000
-load_tile_loop:
-        sta $2118
-    clc
-    adc #$04
-    cmp #$0040
-    bne load_tile_loop
+    lda #$00
+    tay
+tile_loop:
+        pha
+        tya
+        rep #$20 ; 16-bit a
+        and #$00ff
+        asl
+        asl
+        asl
+        asl
+        asl
+        asl
+        sta $2116
+        sep #$20 ; 8-bit a
+        pla
+
+        ldx #$00
+tile_data_loop:
+            sta $2119
+            inc a
+        inx
+        cpx #$08
+        bne tile_data_loop
+    iny
+    cpy #$20
+    bne tile_loop
 
     ; Reset vars
     sep #$20 ; 8-bit a
@@ -192,94 +218,79 @@ vblank:
     lda #$08
     sta $2100
 
+    ; Clear palette
+    sep #$30 ; 8-bit a/x/y
+    stz $2121
+    ldx #$00
+palette_loop:
+        stz $2122
+        stz $2122
+    inx
+    bne palette_loop
+
     ; Prep vars/regs
     sep #$30 ; 8-bit a, x, y
+    lda bar_pos
+    sta bar_pos_temp
     inc bar_pos
-
-    ; Clear character data
-    sep #$30 ; 8-bit a/x/y
-    ldx #$00
-clear_char_loop:
-        stz ram_chars, x
-    inx
-    cpx #$20
-    bne clear_char_loop
-
-    ; Upload chars to VRAM
-    ;  We'll want to clear the first two bytes of every 4th char. ram_chars stores each of these two-byte pairs consecutively, so we'll
-    ;  upload them as single 16-bit writes with 64-byte address increments in between via DMA.
-    sep #$30 ; 8-bit a/x/y
-    lda #$81
-    sta $2115
-
-    stz $2116
-    stz $2117
-
-    lda #$01
-    sta $4320
-    lda #$18
-    sta $4321
-    lda #<ram_chars
-    sta $4322
-    lda #>ram_chars
-    sta $4323
-    stz $4324
-    lda #$20
-    sta $4325
-    stz $4326
-
-    ; Enable DMA
-    lda #$04
-    sta $420b
-
-    ; Dummy write (TODO: Remove when HDMA writes work)
-    sep #$30 ; 8-bit a/x/y
-    lda #$81
-    sta $2115
-
-    lda #$80
-    sta $2116
-    lda #$00
-    sta $2117
-
-    lda #$aa
-    sta $2118
-    lda #$aa
-    sta $2119
-
-    ; Set up VRAM addr/data HDMA channel
-    sep #$20 ; 8-bit a
-    lda #$04
-    sta $4300
-    lda #$16
-    sta $4301
-    lda #<vram_addr_data_hdma_table
-    sta $4302
-    lda #>vram_addr_data_hdma_table
-    sta $4303
-    stz $4304
 
     ; Set up scroll HDMA channel
     sep #$20 ; 8-bit a
     lda #$02
-    sta $4310
+    sta $4300
     lda #$0e
-    sta $4311
+    sta $4301
     lda #<scroll_hdma_table
-    sta $4312
+    sta $4302
     lda #>scroll_hdma_table
+    sta $4303
+    stz $4304
+
+    ; Set up CGRAM addr HDMA table
+    sep #$20 ; 8-bit a
+    lda #$00
+    sta $4310
+    lda #$21
+    sta $4311
+    lda #<cgram_addr_hdma_table
+    sta $4312
+    lda #>cgram_addr_hdma_table
     sta $4313
     stz $4314
 
+    ; Set up CGRAM data 1 HDMA table
+    sep #$20 ; 8-bit a
+    lda #$02
+    sta $4320
+    lda #$22
+    sta $4321
+    lda #<cgram_data_1_hdma_table
+    sta $4322
+    lda #>cgram_data_1_hdma_table
+    sta $4323
+    stz $4324
+
+    ; Set up CGRAM data 2 HDMA table
+    sep #$20 ; 8-bit a
+    lda #$02
+    sta $4330
+    lda #$22
+    sta $4331
+    lda #<cgram_data_2_hdma_table
+    sta $4332
+    lda #>cgram_data_2_hdma_table
+    sta $4333
+    stz $4334
+
     ; Enable HDMA
-    lda #$03
+    lda #$0f
     sta $420c
 
     ; Build scroll HDMA table
     sep #$20 ; 8-bit a
     rep #$10 ; 16-bit x/y
     ldx #$0000
-    ldy #$00ff
+    ldy #$ffff
 scroll_hdma_table_loop:
         lda #$01
         sta scroll_hdma_table, x
@@ -295,28 +306,59 @@ scroll_hdma_table_loop:
     bne scroll_hdma_table_loop
     stz scroll_hdma_table, x
 
-    ; Build VRAM addr/data HDMA table
+    ; Build CGRAM addr HDMA table
     sep #$20 ; 8-bit a
     rep #$10 ; 16-bit x/y
     ldx #$0000
-vram_addr_data_hdma_table_loop:
+cgram_addr_hdma_table_loop:
         lda #$01
-        sta vram_addr_data_hdma_table, x
+        sta cgram_addr_hdma_table, x
+        inx
+        lda bar_pos_temp
+        sta cgram_addr_hdma_table, x
+        inc bar_pos_temp
+        inc bar_pos_temp
+        inc bar_pos_temp
+        inx
+    cpx #(224 * 2)
+    bne cgram_addr_hdma_table_loop
+    stz cgram_addr_hdma_table, x
+
+    ; Build CGRAM data 1 HDMA table
+    sep #$20 ; 8-bit a
+    rep #$10 ; 16-bit x/y
+    ldx #$0000
+cgram_data_1_hdma_table_loop:
+        lda #$01
+        sta cgram_data_1_hdma_table, x
         inx
         rep #$20 ; 16-bit a
-        lda #$0000
-        sta vram_addr_data_hdma_table, x
-        inx
-        inx
-        rep #$20 ; 16-bit a
-        lda #$5555
-        sta vram_addr_data_hdma_table, x
+        lda #$4210
+        sta cgram_data_1_hdma_table, x
         inx
         inx
         sep #$20 ; 8-bit a
-    cpx #(224 * 5)
-    bne vram_addr_data_hdma_table_loop
-    stz vram_addr_data_hdma_table, x
+    cpx #(224 * 3)
+    bne cgram_data_1_hdma_table_loop
+    stz cgram_data_1_hdma_table, x
+
+    ; Build CGRAM data 2 HDMA table
+    sep #$20 ; 8-bit a
+    rep #$10 ; 16-bit x/y
+    ldx #$0000
+cgram_data_2_hdma_table_loop:
+        lda #$01
+        sta cgram_data_2_hdma_table, x
+        inx
+        rep #$20 ; 16-bit a
+        lda #$7fff
+        sta cgram_data_2_hdma_table, x
+        inx
+        inx
+        sep #$20 ; 8-bit a
+    cpx #(224 * 3)
+    bne cgram_data_2_hdma_table_loop
+    stz cgram_data_2_hdma_table, x
 
     ; Reset screen brightness
     sep #$20 ; 8-bit a
